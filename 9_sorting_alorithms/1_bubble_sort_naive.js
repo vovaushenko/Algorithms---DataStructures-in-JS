cfc59d11a0717d514521df0edd721f7d50d9ba56
vovaushenko <vovaushenko1989@gmail.com>
2020-10-31 09:05:54 +0200

    Bubble sort // naive and optimized

---
+++ 9_sorting_alorithms/1_bubble_sort_naive.js
@@ -0,0 +1,15 @@
+// Naive bubble sort
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] > arr[j + 1]) {
+        //swap
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
---
+++ 9_sorting_alorithms/2_bubble_sort_optimized.js
@@ -0,0 +1,16 @@
+//Optimized to reduce the number of iterations
+
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      console.log(arr, arr[j], arr[j + 1]);
+      if (arr[j] > arr[j + 1]) {
+        //swap
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
