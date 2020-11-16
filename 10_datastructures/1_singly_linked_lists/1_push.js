a8f88f985a728db40fa0876358088e9603f7da07
vovaushenko <vovaushenko1989@gmail.com>
2020-11-16 08:58:53 +0200

    SLL

---
+++ 10_datastructures/1_singly_linked_lists/1_push.js
@@ -0,0 +1,41 @@
+// piece of data - val
+//reference to next node - next
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor(params) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    // return this is to return whole list
+    return this;
+  }
+}
+
+// var first = new Node('Hi');
+// first.next = new Node('there')
+// first.next.next = new Node('how')
+// first.next.next.next = new Node('are')
+// first.next.next.next.next = new Node('you')
+
+let list = new SinglyLinkedList();
+// list.push('hello');
+// list.push('goodbye');
