849545590987b5d53d9f6ff8879f937c3421554c
vovaushenko <vovaushenko1989@gmail.com>
2020-11-17 09:03:36 +0200

    introduced pop() method on sll

---
+++ 10_datastructures/1_singly_linked_lists/2_pop.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor(params) {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  //******Push********
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    // return this is to return whole list
+    return this;
+  }
+
+  //******Traversing********
+
+  // traverse() {
+  //   let current = this.head;
+  //   while (current) {
+  //     console.log(current.val);
+  //     current = current.next;
+  //   }
+  // }
+
+  // ********PoP*********
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+}
