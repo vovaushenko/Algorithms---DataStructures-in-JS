7706e83309f603082ab41a4e4d14c4ee5d6a3e95
vovaushenko <vovaushenko1989@gmail.com>
2020-11-19 17:58:19 +0200

    Added shift() method

--- 10_datastructures/1_singly_linked_lists/3_shift.js
+++ 10_datastructures/1_singly_linked_lists/3_shift.js
@@ -55,4 +55,16 @@ class SinglyLinkedList {
     }
     return current;
   }
+
+  //********Shift*********
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
 }
