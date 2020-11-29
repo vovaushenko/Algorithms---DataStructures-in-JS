2265a0b4b793c4e99de6e2ab69195b1e2ee1e7a0
vovaushenko <vovaushenko1989@gmail.com>
2020-11-29 18:44:01 +0200

    Added shift() method

---
+++ 10_datastructures/2_doubly_linked_list/3_shift.js
@@ -0,0 +1,75 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  traverse() {
+    if (this.length === 0) return 'The list is empty';
+    let current = this.head;
+    let route = '';
+    while (current) {
+      current.next
+        ? (route += `${current.val} <-> `)
+        : (route += `${current.val}`);
+      current = current.next;
+    }
+    return route;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+
+    let poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+
+    return poppedNode;
+  }
+
+  shift() {
+    if (!this.head) return undefined;
+    let oldHead = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = oldHead.next;
+      this.head.prev = null;
+      oldHead.next = null;
+    }
+    this.length--;
+
+    return oldHead;
+  }
+}
