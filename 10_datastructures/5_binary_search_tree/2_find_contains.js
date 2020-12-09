f91c9a4dbc71c7f40a4d7df03de1fe4f76a7181d
vovaushenko <vovaushenko1989@gmail.com>
2020-12-09 08:49:32 +0200

    Added Search && Contains methods to BST

--- 10_datastructures/5_binary_search_tree/2_find_contains.js
+++ 10_datastructures/5_binary_search_tree/2_find_contains.js
@@ -38,17 +38,39 @@ class BinarySearchTree {
       }
     }
   }
+
+  find(value) {
+    if (!this.root) return false;
+    let current = this.root;
+    let found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return false;
+    return current;
+  }
+
+  contains(value) {
+    if (!this.root) return false;
+    let current = this.root;
+    let found = false;
+
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
 }
-let tree = new BinarySearchTree();
-
-//          10
-//   5             13
-// 2    7        11    16
-
-tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-tree.insert(7);
