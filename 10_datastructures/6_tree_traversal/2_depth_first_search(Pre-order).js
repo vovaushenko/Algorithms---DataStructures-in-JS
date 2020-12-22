57d97ed3e626b47b9a078c0fac54db6c62cdea6a
vovaushenko <vovaushenko1989@gmail.com>
2020-12-22 09:39:11 +0200

    Added DFS post order

--- 10_datastructures/6_tree_traversal/2_depth_first_search(Pre-order).js
+++ 10_datastructures/6_tree_traversal/2_depth_first_search(Pre-order).js
@@ -102,4 +102,18 @@ class BinarySearchTree {
     traverse(current);
     return data;
   }
+
+  DFSPostOrder() {
+    let values = [];
+    let current = this.root;
+
+    const traverse = (node) => {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    };
+
+    traverse(current);
+    return data;
+  }
 }
