84a783003398cb0dbb25e20e53b30e79045fa2c1
vovaushenko <vovaushenko1989@gmail.com>
2020-12-26 10:56:10 +0200

    Hash table - set() and get() methods

---
+++ 10_datastructures/10_hash_tables/2_hash_table_set&get.js
@@ -0,0 +1,38 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  // 1) set accepts key and value -> 2) hashes the key -> 3) stores key-value pair in the hash-table array via spread chaining
+
+  set(key, value) {
+    let index = this._hash(key);
+    if (!this.keyMap[index]) {
+      this.keyMap[index] = [];
+    }
+    this.keyMap[index].push([key, value]);
+  }
+
+  // 1) get accepts a key 2) hashes the key 3) retrieves the key-value pair 4)if key isn't found, returns undefined
+  get(key) {
+    let index = this._hash(key);
+    if (this.keyMap[index]) {
+      for (let i = 0; i < this.keyMap[index].length; i++) {
+        if (this.keyMap[index][i][0] === key) return this.keyMap[index][i][1];
+      }
+    }
+
+    return undefined;
+  }
+}
